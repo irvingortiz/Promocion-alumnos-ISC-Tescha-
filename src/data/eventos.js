fb81380c444a1949fa0d4d23e0a42e6568a3d01f
Tu Nombre <tu_email@ejemplo.com>
2026-07-09 13:52:38 -0600

    actualizacion de imagenes

---
+++ src/data/eventos.js
@@ -0,0 +1,28 @@
+// src/data/eventos.js
+export const eventos = [
+  {
+    titulo: "Innovatec",
+    descripcion: "Proyectos de innovación tecnológica.",
+    fotos: ["/fotos/innovatec/1.jpg", "/fotos/innovatec/2.jpg", "/fotos/innovatec/3.jpg", "/fotos/innovatec/4.jpg", "/fotos/innovatec/5.jpg", "/fotos/innovatec/6.jpg", "/fotos/innovatec/7.jpg"]
+  },
+  {
+    titulo: "Flisol",
+    descripcion: "Festival de software libre.",
+    fotos: ["/fotos/flisol-2026/1.jpg", "/fotos/flisol-2026/2.jpg" , "/fotos/flisol-2026/3.jpg", "/fotos/flisol-2026/4.jpg", "/fotos/flisol-2026/5.jpg"]
+  },
+  {
+    titulo: "Wolftec",
+    descripcion: "Ingeniería en Sistemas Computacionales participó en el “AXOLOTL TOURNAMENT 2026",
+    fotos: ["/fotos/wolftec/1.jpg", "/fotos/wolftec/2.jpg", "/fotos/wolftec/3.jpg", "/fotos/wolftec/4.jpg"]
+  },
+  {
+    titulo: "Proyecto Integrador",
+    descripcion: "En el marco del aprendizaje situado y la vinculación con el sector productivo",
+    fotos: ["/fotos/integrador/1.jpg", "/fotos/integrador/2.jpg", "/fotos/integrador/3.jpg", "/fotos/integrador/4.jpg"]
+  },
+  {
+    titulo: "Director General",
+    descripcion: "Encuentro con el Director General",
+    fotos: ["/fotos/director/1.jpg", "/fotos/director/2.jpg"]
+  }
+];
